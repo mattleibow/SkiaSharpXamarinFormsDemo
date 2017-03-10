6b056d2643584f4285a2f657dc4dfb8351750c38
Matthew Leibowitz <mattleibow@live.com>
2017-03-10 21:12:16 +0200

    Added a sample to compare Xamarin.Forms, Native, SkiaSharp text

---
+++ SkiaSharpXamarinFormsDemo/SkiaSharpXamarinFormsDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace SkiaSharpXamarinFormsDemo
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
---
+++ SkiaSharpXamarinFormsDemo/SkiaSharpXamarinFormsDemo/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace SkiaSharpXamarinFormsDemo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+#if __ANDROID__
+			var androidView = stack.Children.OfType<Xamarin.Forms.Platform.Android.NativeViewWrapper>().FirstOrDefault();
+			var textView = androidView?.NativeView as Android.Widget.TextView;
+			textView?.SetMaxLines(1);
+#endif
+		}
+
+		private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+		{
+			var scale = e.Info.Height / ((SKCanvasView)sender).Height;
+
+			var canvas = e.Surface.Canvas;
+
+			canvas.Clear(SKColors.Transparent);
+
+			var paint = new SKPaint
+			{
+				TextSize = (float)(40 * scale),
+				Typeface = SKTypeface.FromFamilyName("Arial"),
+				IsAntialias = true
+			};
+
+			var text = "Hello World! (SkiaSharp)";
+
+			var bounds = SKRect.Empty;
+			paint.MeasureText(text, ref bounds);
+
+			canvas.DrawText(text, 0, -bounds.Top, paint);
+		}
+	}
+}
